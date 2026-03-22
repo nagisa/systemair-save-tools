eb1e4238003b1a047be437daff40bb0a2f748e3f
Simonas Kazlauskas <git@kazlauskas.me>
2026-03-22 17:41:06 +0200

    homie: fix filter time reset implementation
    Weirdly, `FILTER_PERIOD_SET` despite its description is a read-only
    register and does not appear to do anything at all. In order to reset
    the filter timing you have to set `FILTER_PERIOD` to the number of
    months in which the filter needs to be replaced next… That also,
    unfortunately, means you cannot really adjust this parameter without
    additional side-effects, which maybe suggests that it should be exposed
    in some other way?
    
    That said, I'm finding myself wanting a sub-command that could modify
    this without having to go through MQTT...

--- src/homie/filter_node.rs
+++ src/homie/filter_node.rs
@@ -115,9 +115,31 @@ impl ActionPropertyValue for ReplaceAction {
         modbus: std::sync::Arc<crate::connection::Connection>,
     ) -> std::pin::Pin<Box<super::EventStream>> {
         Box::pin(async_stream::stream! {
-            let register = const { RegisterIndex::from_name("FILTER_PERIOD_SET").unwrap() };
+            // Resetting the filter period can happen via setting of the `FILTER_PERIOD` to the
+            // next replacement deadline in months. We have this value cached in the background,
+            // but it is inconvenient to get to said cache. So we'll "just" read it out manually.
+            let register = const { RegisterIndex::from_name("FILTER_PERIOD").unwrap() };
             let address = register.address();
-            let operation = modbus::Operation::SetHoldings { address, values: vec![1] };
+            let operation = modbus::Operation::GetHoldings { address, count: 1 };
+            let response = modbus.send_retrying(operation.clone()).await?;
+            let modbus::ResponseKind::GetHoldings { values } = response.kind else {
+                yield Ok(EventResult::ActionFailure {
+                    node_id,
+                    prop_idx,
+                    why: "could not read original value for FILTER_PERIOD register"
+                });
+                return;
+            };
+            let Some(value) = values.as_array::<2>() else {
+                yield Ok(EventResult::ActionFailure {
+                    node_id,
+                    prop_idx,
+                    why: "FILTER_PERIOD read responded with wrong number of bytes?"
+                });
+                return;
+            };
+            let value = u16::from_be_bytes(*value);
+            let operation = modbus::Operation::SetHoldings { address, values: vec![value] };
             let _response = modbus.send_retrying(operation.clone()).await?.kind;
             // SUBTLE: _response could be a server exception. We still signal to the action invoker
             // that we received the command. They will be able to tell if this had a desired effect
@@ -127,7 +149,7 @@ impl ActionPropertyValue for ReplaceAction {
                 prop_idx,
                 value: Box::new(Self)
             });
-            let operation = modbus::Operation::GetHoldings { address, count: 3 };
+            let operation = modbus::Operation::GetHoldings { address, count: 7 };
             let response = modbus.send_retrying(operation.clone()).await?.kind;
             yield Ok(EventResult::HomieSet { node_id, prop_idx, operation, response });
         })
